423368268d9cbecc3d2f3d0a81667bb3158671b5
test119770808 <test119770808@gmail.com>
2024-12-27 18:01:29 +0900

    ch6 spring-data

---
+++ sbur-neo4j/src/main/java/com/thehecklers/sburneo4j/SburNeo4jApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.sburneo4j;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburNeo4jApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SburNeo4jApplication.class, args);
+    }
+
+}
