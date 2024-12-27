c715ecbe218c4b2a702914a869aaf56172568892
test119770808 <test119770808@gmail.com>
2024-12-27 12:39:20 +0900

    ch6 spring-data - redis repository 사용 및 jpa 모듈 생성

---
+++ sburjpa/src/main/java/com/thehecklers/sburjpa/SburjpaApplication.java
@@ -0,0 +1,13 @@
+package com.thehecklers.sburjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SburjpaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SburjpaApplication.class, args);
+    }
+
+}
